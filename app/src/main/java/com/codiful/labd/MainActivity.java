33e76300537d80baa652efa4949a8a2f9e9d16a4
anehme <anehme139@gmail.com>
2020-03-16 19:09:06 +0200

    publish

--- app/src/main/java/com/codiful/labd/MainActivity.java
+++ app/src/main/java/com/codiful/labd/MainActivity.java
@@ -2,43 +2,54 @@ package com.codiful.labd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-private Button btnlogin;
-private EditText username;
-private EditText password;
-
+    private ListView listOfCars;
+    private static CarAdapter carAdapter;
+    private EditText addNewCar;
+    private Button addButton;
+    private ArrayList<Car> c ;
     private void init(){
-        btnlogin = (Button) findViewById(R.id.loginbtn);
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById((R.id.password));
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        addNewCar =  (EditText) findViewById(R.id.editText);
+        addButton = (Button) findViewById(R.id.btn);
+
+        listOfCars = (ListView) findViewById(R.id.lv_list_cars);
+        carAdapter = new CarAdapter(MainActivity.this);
+        carAdapter.addCar("Dacia",R.drawable.lab_car_icon);
+        carAdapter.addCar("Dacia",R.drawable.lab_car_icon);
+
+        carAdapter.addCar("Dacia",R.drawable.lab_car_icon);
+
+        carAdapter.addCar("Dacia",R.drawable.lab_car_icon);
+        carAdapter.notifyDataSetChanged();
+        listOfCars.setAdapter(carAdapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("student") && password.getText().toString().equals("student"))
-                {
-                    Intent i = new Intent(MainActivity.this,FindTheNumberActivity.class);
-                    i.putExtra("username","student");
-                    startActivity(i);
-                }
+                carAdapter.addCar(addNewCar.getText().toString(),R.drawable.lab_car_icon);
             }
         });
+
+
     }
 
 
---
+++ app/src/main/java/com/codiful/labd/TagCar.java
@@ -0,0 +1,9 @@
+package com.codiful.labd;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public  class TagCar {
+     TextView nametxt;
+     ImageView img;
+}
