958d5ad30727b38ca7b307e2b451903db62f4aa0
anehme <anehme139@gmail.com>
2020-03-30 17:02:12 +0300

    second commit

--- app/src/main/java/com/codiful/labd/MainActivity.java
+++ app/src/main/java/com/codiful/labd/MainActivity.java
@@ -1,109 +1,43 @@
 package com.codiful.labd;
 
+
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button Mic;
-    private Button Listen;
-    private TextView txt;
-    private TextToSpeech tts;
-    private final int REQ_CODE_SPEECH_INPUT = 100;
 
-    private void initView() {
-        Mic = (Button) findViewById(R.id.Mic);
-        Listen = (Button) findViewById(R.id.listen);
-        txt = (TextView) findViewById(R.id.txt);
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    tts.setLanguage(Locale.US);
-                }
-            }
-        });
-    }
 
-    private void speechConfig() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                "Say something");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(), "Not supported ", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txt.setText(result.get(0));
-                }
-                break;
-            }
-
-        }
-    }
-
-    private void OpenMic() {
-        Mic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                speechConfig();
-            }
-        });
-    }
-
-    private void Listen() {
-        Listen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!txt.getText().toString().equals("")) {
-                    tts.speak(txt.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
-                }
-            }
-        });
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-
-        OpenMic();
-        Listen();
 
     }
+    public void selectFrag(View view) {
+        Fragment fr;
 
+        if(view == findViewById(R.id.button2)) {
+            fr = new FragmentTwo();
+
+        }else {
+            fr = new FragmentOne();
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
 
 }
