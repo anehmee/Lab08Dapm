49e398ee3e0757c115c4833e700c582fb807f002
anehme <anehme139@gmail.com>
2020-03-30 16:18:45 +0300

    First deploy

--- app/src/main/java/com/codiful/labd/MainActivity.java
+++ app/src/main/java/com/codiful/labd/MainActivity.java
@@ -4,47 +4,92 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button save;
-    private Button activity;
-    private EditText input;
-    private MySharedPreference sh;
+    private Button Mic;
+    private Button Listen;
+    private TextView txt;
+    private TextToSpeech tts;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     private void initView() {
-        save = (Button) findViewById(R.id.save);
-        activity = (Button) findViewById(R.id.btn2);
-        input = (EditText) findViewById(R.id.editText);
-
-        sh = new MySharedPreference(getApplicationContext());
-    }
-
-    private void SaveData() {
-        save.setOnClickListener(new View.OnClickListener() {
+        Mic = (Button) findViewById(R.id.Mic);
+        Listen = (Button) findViewById(R.id.listen);
+        txt = (TextView) findViewById(R.id.txt);
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
-            public void onClick(View v) {
-                sh.save(input.getText().toString());
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+                }
             }
         });
     }
 
-    private void OpenActivity() {
-        activity.setOnClickListener(new View.OnClickListener() {
+    private void speechConfig() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "Not supported ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txt.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+    private void OpenMic() {
+        Mic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, SecondActivity.class);
-                i.putExtra("no_value","Oops ! no data saved , this message is from intent");
-                startActivity(i);
+                speechConfig();
+            }
+        });
+    }
+
+    private void Listen() {
+        Listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!txt.getText().toString().equals("")) {
+                    tts.speak(txt.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+                }
             }
         });
     }
@@ -55,8 +100,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
 
-        SaveData();
-        OpenActivity();
+        OpenMic();
+        Listen();
 
     }
 
